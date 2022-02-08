90af01c287bd5cc4efdd44e30cd816f5ec5a9ba6
sebbmn <sebbmn@users.noreply.github.com>
2022-02-08 18:42:12 +0100

    refactor: reorganise display & init logic II

--- js/script.js
+++ js/script.js
@@ -1,22 +1,38 @@
 const video = document.createElement('video');
-const playPauseButton = document.createElement('button');
 
-const printedFrames = document.getElementById('printed-frames');
 const videoNavigation = document.getElementById('video-navigation');
+const printedFrames = document.getElementById('printed-frames');
 const cursor = document.getElementById('cursor');
 
-let intViewportWidth, intViewportHeight, isPlaying, maxTime, pages, pageNumber, frame, maxFrames, unitSize;
+let isPlaying = false;
+let maxTime = 0;
+
+let pages = [];
+let pageNumber = 0;
+let frame = 0;
+let maxFrames = getMaxFrames();
 
 document.addEventListener('DOMContentLoaded', () => {
   initVideo();
-  initVariables();
-  setElementsDimensions();
   fillVideoNavigationHTML();
   fillPlayPauseButtonHTML();
+  setElementsDimensions();
   initListeners();
 });
 
-function getDimensions () {
+function initVideo() {
+  video.src = 'media/photosynthesis.mp4'; //https://ia903101.us.archive.org/15/items/photosynthesis_201911/photosynthesis.mp4
+  video.id = 'video';
+}
+
+function getMaxFrames() {
+  const dimensions = getDimensions()
+  const framesPerLine = Math.trunc(dimensions.printedFramesWidth / dimensions.unitSizePx);
+
+  return framesPerLine * 20;
+}
+
+function getDimensions() {
   intViewportWidth = window.innerWidth;
   intViewportHeight = window.innerHeight;
 
@@ -30,13 +46,10 @@ function getDimensions () {
   return dimensions;
 }
 
-function initVideo() {
-  video.src = 'media/photosynthesis.mp4'; //https://ia903101.us.archive.org/15/items/photosynthesis_201911/photosynthesis.mp4
-  video.id = 'video';
-}
-
 function setElementsDimensions() {
   const dimensions = getDimensions();
+  // const navigationFrameList = document.getElementsByClassName('navigation-frame');
+  // const printedFrameList = document.getElementsByClassName('printed-frame');
 
   videoNavigation.style = `height:${(dimensions.contentHeight)}px;width:${dimensions.videoNavigationWidth}px`;
   printedFrames.style = `height:${(dimensions.contentHeight)}px;width:${dimensions.printedFramesWidth}px`;
@@ -48,17 +61,19 @@ function fillVideoNavigationHTML() {
   const dimensions = getDimensions();
   const size = 4 * dimensions.unitSizePx;
 
-  for(i=0;i<10;i++) {
+  for(i = 0;i < 10;i++) {
     const newDiv = document.createElement('div');
     newDiv.id = `navigation-frame-${i}`;
     newDiv.className = 'navigation-frame';
-    newDiv.style = `background: #${i}A${i}B${i}C;height:${size}px;width:${size}px`;//TODO: move to layout dimensions
+    newDiv.style = `background: #${i}A${i}B${i}C;height:${size}px;width:${size}px`;
     videoNavigation.appendChild(newDiv);
   }
 }
 
 function fillPlayPauseButtonHTML() {
+  const playPauseButton = document.createElement('button');
   playPauseButton.className = 'button';
+  playPauseButton.id = 'play-pause-button'
   const navigationFrame = document.getElementById(`navigation-frame-${pageNumber + 1}`);
   navigationFrame.appendChild(playPauseButton);
 
@@ -73,27 +88,9 @@ function fillPlayPauseButtonHTML() {
   });
 }
 
-function initVariables() {
-
-  intViewportWidth = window.innerWidth;
-  intViewportHeight = window.innerHeight;
-
-  isPlaying = false;
-  maxTime = 0;
-
-  pages = [];
-  pageNumber = 0;
-  frame = 0;
-
-  unitSize = Math.trunc((intViewportHeight - 50) / 20);
-}
-
-
-
 function initListeners() {
   window.addEventListener('resize', () => {
-    intViewportWidth = window.innerWidth;
-    intViewportHeight = window.innerHeight;
+    setElementsDimensions();
   });
   
   video.addEventListener('play', function() {
@@ -124,6 +121,7 @@ function newPage() {
   const currentNavFrame = document.getElementById(`navigation-frame-${pageNumber}`);
   const newNavFrame = document.getElementById(`navigation-frame-${pageNumber + 1}`);
   const nextNavFrame = document.getElementById(`navigation-frame-${pageNumber + 2}`);
+  const playPauseButton = document.getElementById('play-pause-button')
   const oldPageItems = [];
 
   newNavFrame.removeChild(newNavFrame.firstChild);
@@ -161,7 +159,7 @@ function navigateToPage(index) {
 function refreshNavigationFrame() {
   const navigationFrame = document.getElementById(`navigation-frame-${pageNumber}`);
   const navigationFrameCanvas = document.createElement("canvas");
-  const canvasSize = 4 * unitSize;
+  const canvasSize = 4 * getDimensions().unitSizePx;
 
   navigationFrameCanvas.width = canvasSize;
   navigationFrameCanvas.height = canvasSize;
@@ -180,7 +178,8 @@ function refreshNavigationFrame() {
 /* */
 function printFrame(currentTime) {
   const printedFrameCanvas = document.createElement("canvas");
-  const canvasSize = 4 * unitSize;
+  const size = getDimensions().unitSizePx
+  const canvasSize = 4 * size;
 
   printedFrameCanvas.width = canvasSize;
   printedFrameCanvas.height = canvasSize;
@@ -188,7 +187,7 @@ function printFrame(currentTime) {
   const printedFrameContext = printedFrameCanvas.getContext("2d");
   printedFrameContext.drawImage(video,80,0,480,480,0,0,canvasSize,canvasSize);
   printedFrameCanvas.className = "printed-frame";
-  printedFrameCanvas.style = `height:${unitSize}px;width:${unitSize}px;`;
+  printedFrameCanvas.style = `height:${size}px;width:${size}px;`;
 
   printedFrameCanvas.onclick = () => { 
     video.currentTime = currentTime;
